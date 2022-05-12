c9c25eb43e0dce4bdfc2613123fb19fe6231ea22
Naveen95-clt <naveenpm9@gmail.com>
2022-05-12 22:25:14 +0530

    two

---
+++ script.js
@@ -0,0 +1,27 @@
+let button = document.getElementById('button')
+ 
+let name,password,message
+ 
+button.addEventListener('click',() => {
+  
+    name = document.getElementById('name').value
+ 
+    password = document.getElementById('password').value
+ 
+    let message = (alertmessage) => {
+        alert(alertmessage)
+    }
+ 
+    validateform(name,password,message)
+ 
+ 
+})
+ 
+let validateform = (name,password,callback) => {
+    if(name == "username" && password == 12345){
+        window.open("index2.html")
+    }
+    else{
+        callback("Form is not validated")
+    }
+}
